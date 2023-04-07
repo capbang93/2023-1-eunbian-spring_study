aa116bb35205448944f0c2ad4afd7900067e243b
EunbiAn <qldms133@naver.com>
2023-04-07 09:32:09 +0900

    h2에서 마리아디비로 데이터베이스 변경

--- src/main/java/com/example/Todo/model/UserEntity.java
+++ src/main/java/com/example/Todo/model/UserEntity.java
@@ -13,7 +13,7 @@ import javax.persistence.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@Table(name="user", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
 public class UserEntity {
     @Id
     @GeneratedValue(generator = "system-uuid")
